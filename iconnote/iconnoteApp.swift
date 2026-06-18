25181d0c1f48d31369338135d8ef4ef297a6b0d1
Sandor Kromesch <sandor.kromesch@erise.hu>
2026-06-18 21:19:26 +0200

    feat: wire AppDelegate to SwiftUI app entry point

--- iconnote/iconnoteApp.swift
+++ iconnote/iconnoteApp.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 @main
 struct iconnoteApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
+        // Empty Settings scene satisfies SwiftUI lifecycle requirement.
+        // All UI is managed by AppDelegate via NSStatusItem + NSPopover.
         Settings { EmptyView() }
     }
 }
